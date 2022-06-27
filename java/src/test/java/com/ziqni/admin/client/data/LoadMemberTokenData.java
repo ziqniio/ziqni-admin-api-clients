3b94125da2668c36f26d71c694cfaebd73a4a195
gpfunguro <Given.Pfunguro@cassavasmartech.co.zw>
2022-06-27 17:12:11 +0200

    update on MemberToken Test

--- java/src/test/java/com/ziqni/admin/client/data/LoadMemberTokenData.java
+++ java/src/test/java/com/ziqni/admin/client/data/LoadMemberTokenData.java
@@ -5,15 +5,15 @@ import tests.utils.CompleteableFutureTestWrapper;
 
 public class LoadMemberTokenData implements CompleteableFutureTestWrapper {
 
-    public MemberTokenRequest getMemberTokenRequest(String member, String apiKey, boolean isReferenceId, int expires) {
-
+    public MemberTokenRequest getMemberTokenRequest(String member, String apiKey, String resource, boolean isReferenceId, int expires) {
         return new MemberTokenRequest()
                 .member(member)
                 .apiKey(apiKey)
+                .resource(resource)
                 .isReferenceId(isReferenceId)
                 .expires(expires);
 
-                    }
+    }
 
 
 }
